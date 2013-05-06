55e7ac7e95945d41929c463467d304ee3b92bfd6
IvanovaAnastasiya <nastechka_iva@mail.ru>
2013-05-06 10:42:00 +0600

    с bootstrap

--- KinoZal/JavaScript.js
+++ KinoZal/JavaScript.js
@@ -1,8 +1,7 @@
-﻿# knockout-2.2.1.js
+﻿
        function ViewModel11() {
            var self = this;
            self.items = ko.observableArray([{ id: 1, text: "1" }, { id: 2, text: "2" }, { id: 3, text: "2" }, { id: 4, text: "2" }, { id: 5, text: "2" }]);
            return self;
        }
 
-ko.applyBindings(new ViewModel11());
